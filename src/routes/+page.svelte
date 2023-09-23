342f22fe72333f6a9de4a0e5d61b408fbcb91346
ivan wolyniec <ivanwolyniec@gmail.com>
2023-09-23 10:57:03 -0400

    2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<h1>this is ivan's portfolio</h1>
+<h1>this is ivan's portfolio 2</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
